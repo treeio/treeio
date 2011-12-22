fc81f125800aee6c47c11c8bd9d3f85210066184
Cyril <Cyril Robert cyrilrbt@gmail.com>
2011-12-22 12:20:16 -0500

    Fixed traceback in initial Documents component by allowing nulls in Document.folder

--- documents/models.py
+++ documents/models.py
@@ -20,12 +20,12 @@ class Folder(Object):
     """ Every folder may have a parent folder """
     name = models.CharField(max_length=255)
     parent = models.ForeignKey('self', blank=True, null=True, related_name='child_set')
-    
+
     access_inherit = ('parent', '*module', '*user')
-    
+
     def __unicode__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         "Returns absolute URL of the object"
         try:
@@ -39,36 +39,36 @@ def generate_filename(instance, old_filename):
     filename = str(time.time()) + extension
     return 'documents/files/' + filename
 
-    
+
 # File model
 class File(Object):
     """ A binary or other non-renderable file (i.e. an image) """
     name = models.CharField(max_length=255)
     folder = models.ForeignKey(Folder)
     content = models.FileField(upload_to=generate_filename, storage=FileStorage())
-    
+
     access_inherit = ('folder', '*module', '*user')
-    
+
     def __unicode__(self):
         return self.name
-    
+
     def get_file_type(self):
         match = re.match('.*\.(?P<extension>[a-z]+)', unicode(self.content))
         if match:
             return unicode(match.group('extension')).upper()
         else:
             return ''
-    
+
     def can_preview(self):
         filetype = self.get_file_type()
         for ext in ('PNG', 'JPG', 'JPEG', 'BMP', 'GIF', 'SVG'):
             if filetype == ext:
                 return True
         return False
-    
+
     def get_preview_url(self):
         return getattr(settings, 'MEDIA_URL', '/static/media/') + unicode(self.content)
-    
+
     class Meta:
         " File "
         ordering = ['-last_updated']
@@ -79,31 +79,31 @@ class File(Object):
             return reverse('documents_file_view', args=[self.id])
         except Exception:
             return ""
-    
-    
+
+
 # Document model
 class Document(Object):
     """ A readable document, i.e. HTML, which may be rendered directly """
     title = models.CharField(max_length=255)
-    folder = models.ForeignKey(Folder)
+    folder = models.ForeignKey(Folder, null=True, blank=True)
     body = models.TextField(null=True, blank=True)
 
     access_inherit = ('folder', '*module', '*user')
-    
+
     def __unicode__(self):
         return self.title
 
     class Meta:
         " File "
         ordering = ['-last_updated']
-    
+
     def get_absolute_url(self):
         "Returns absolute URL of the object"
         try:
             return reverse('documents_document_view', args=[self.id])
         except Exception:
             return ""
-    
+
 # WebLink model
 class WebLink(Object):
     """ A web link """
@@ -112,15 +112,15 @@ class WebLink(Object):
     url = models.CharField(max_length=255)
 
     access_inherit = ('folder', '*module', '*user')
-    
-    
+
+
     def __unicode__(self):
         return self.title
-    
+
     class Meta:
         " File "
         ordering = ['-last_updated']
-        
+
     def get_absolute_url(self):
         "Returns absolute URL of the object"
         try:
