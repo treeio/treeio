31d0abb0722faedc5961ae2db22642ddfde68fb9
Adam Awan <adamja1@gmail.com>
2012-03-03 23:58:53 +0000

    Added command file

---
+++ core/management/commands/related_fields_patch.py
@@ -0,0 +1,9 @@
+#Django command helper to apply django related fields patch programattically
+import django
+import os
+from subprocess import Popen, PIPE
+
+patch_file = os.path.join(os.path.abspath('.'),'bin','django-related-fields.patch')
+dir_to_file_in_need_of_patch = os.path.dirname(django.__file__)
+process = Popen(['patch','-p1'],stdin=PIPE,shell=False,cwd=dir_to_file_in_need_of_patch)
+process.communicate(open(patch_file).read())
