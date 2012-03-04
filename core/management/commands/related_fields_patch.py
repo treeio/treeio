7ac298e651bc42e1edd402b480a750a833da9107
Adam Awan <adamja1@gmail.com>
2012-03-04 00:08:13 +0000

    Attempt at logging

--- core/management/commands/related_fields_patch.py
+++ core/management/commands/related_fields_patch.py
@@ -6,4 +6,4 @@ from subprocess import Popen, PIPE
 patch_file = os.path.join(os.path.abspath('.'),'bin','django-related-fields.patch')
 dir_to_file_in_need_of_patch = os.path.dirname(django.__file__)
 process = Popen(['patch','-p1'],stdin=PIPE,shell=False,cwd=dir_to_file_in_need_of_patch)
-process.communicate(open(patch_file).read())
+print process.communicate(open(patch_file).read())
