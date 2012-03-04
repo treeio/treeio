20a629c61fd6bc8b02f0fc39c4ef142977715556
Adam Awan <adamja1@gmail.com>
2012-03-04 00:15:39 +0000

    Trying to except the subprocess error

--- core/management/commands/related_fields_patch.py
+++ core/management/commands/related_fields_patch.py
@@ -5,5 +5,9 @@ from subprocess import Popen, PIPE
 
 patch_file = os.path.join(os.path.abspath('.'),'bin','django-related-fields.patch')
 dir_to_file_in_need_of_patch = os.path.dirname(django.__file__)
-process = Popen(['patch','-p1'],stdin=PIPE,shell=False,cwd=dir_to_file_in_need_of_patch)
-print process.communicate(open(patch_file).read())
+try:
+	process = Popen(['patch','-p1'],stdin=PIPE,shell=False,cwd=dir_to_file_in_need_of_patch)
+	process.communicate(open(patch_file).read())
+	process.kill()
+except:
+	pass
