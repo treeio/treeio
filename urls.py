44b07d2f6f12e98aad62a4ace0b03d017f5f1f49
Bear <bear@nuwainfo.com>
2014-12-19 15:59:12 +0800

    Make it able to handle static files if DEBUG = False without Apache or Nginx.

--- urls.py
+++ urls.py
@@ -82,4 +82,16 @@ if 'rosetta' in settings.INSTALLED_APPS:
     urlpatterns += patterns('',
                             url(r'^rosetta/', include('rosetta.urls')),
                             )
-urlpatterns += staticfiles_urlpatterns()
+                            
+if settings.DEBUG:
+    # Dajaxice depends on django.contrib.staticfiles to handle it.
+    # <STATIC_URL>/dajaxice.core.js is rendered by its DajaxiceFinder.
+    urlpatterns += staticfiles_urlpatterns()
+else:
+    # django.contrib.staticfiles won't work if DEBUG = False, we need to handle
+    # static files via django.views.static.serve.
+    urlpatterns +=   patterns('', 
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_DOC_ROOT
+        }),
+    )
