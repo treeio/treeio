c9b73eb8528b263ff3b56bb1117620251c8a74ca
Avraham Seror <tovmeod@gmail.com>
2015-02-07 21:20:22 +0200

    travis will run tests on mysql, postgres and sqlite

--- treeio_project/settings.py
+++ treeio_project/settings.py
@@ -35,7 +35,22 @@ DATABASES = {}
 TESTING = 'test' in sys.argv or 'test_coverage' in sys.argv  #Covers regular testing and django-coverage
 print(sys.argv)
 if TESTING:
-    DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
+    test_db = os.environ.get('DB', 'sqlite')
+    if test_db == 'mysql':
+        DATABASES = {'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'treeio',
+            'USER': 'root',
+            }}
+    elif test_db == 'postgres':
+        DATABASES = {'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'treeio',
+            'USER': 'postgres',
+        }}
+    elif test_db == 'sqlite':
+        DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
+
     PASSWORD_HASHERS = ('django.contrib.auth.hashers.MD5PasswordHasher',)
     HARDTREE_API_AUTH_ENGINE = 'basic'
 else:
