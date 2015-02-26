84c3439a179f1bc124411ea3ed5ed2ebbe8f1b6f
avraham <tovmeod@gmail.com>
2015-02-26 21:17:24 +0200

    added missing template context processor static, necessary for collectstatic

--- treeio_project/settings.py
+++ treeio_project/settings.py
@@ -149,6 +149,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.i18n",
     "django.core.context_processors.debug",
     "django.core.context_processors.media",
+    "django.core.context_processors.static",
     "django.core.context_processors.request",
     'django.contrib.messages.context_processors.messages',
 )
