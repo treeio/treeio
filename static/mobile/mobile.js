69e1edb87f0681ffe4370f376c75751c148ed096
avraham <tovmeod@gmail.com>
2015-04-11 23:32:42 +0300

    removed unnecessary ; added missing ;

--- static/mobile/mobile.js
+++ static/mobile/mobile.js
@@ -8,12 +8,12 @@ $(function() {
   $(window).hashchange(function(event) {
     $('form').each(function() {
       var url = window.location.hash.replace('#', '');
-      if (url == '') { url = window.location.pathname };
+      if (url == '') { url = window.location.pathname }
       if (url.indexOf('?') != -1) {
         url = url.substring(0, url.indexOf('?'));
-      };
+      }
       $(this).attr('action', url);
     })
-  })
+  });
   $(window).hashchange();
 });
